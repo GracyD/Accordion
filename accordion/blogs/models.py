8200e05ce19406d9b0e4a3d0792cf81b316d1dee
SerenityOnly <yanyt523@gmail.com>
2015-01-23 16:52:35 +0800

    Blog model created.

--- accordion/blogs/models.py
+++ accordion/blogs/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+
+class Article(models.Model):
+	article_title = models.CharField(max_length=200)
+	article_body = models.CharField(max_length=2000)
+	article_pub_date = models.DateTimeField('date published')
