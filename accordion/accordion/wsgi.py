55f68653cb7703b07480bec4aeae7ec98c7667f9
SerenityOnly <yanyt523@gmail.com>
2015-01-26 17:42:33 +0800

    Apache wsgi configuration.

--- accordion/accordion/wsgi.py
+++ accordion/accordion/wsgi.py
@@ -12,3 +12,9 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE", "accordion.settings")
 
 from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
+
+"""
+Newly added content. The above content is included in the original wsgi.py file.
+"""
+import sys
+sys.path.append(r'C:\Yannnyit\03_Django_Prac\Accordion\accordion')
